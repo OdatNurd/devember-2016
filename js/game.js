358bed85ba54fdf6f31147b49e2b343b54dee2cd
Terence Martin <poindexter@nurdz.com>
2016-12-06 14:29:14 -0800

    Include a Marker entity
    In order to work on dropping the ball through the maze, we need it to
    work two ways: one step at a time, as well as capturing an entire path
    all at once (for use in AI).
    
    In order to test this out we're going to start with calculating all of
    the steps at once, and so we are implementing a new Marker entity.
    
    This does nothing but render it's bounds for debugging. We will use it
    to mark what path the ball takes on it's path.

--- js/game.js
+++ js/game.js
@@ -178,6 +178,44 @@ var nurdz;
     var game;
     (function (game) {
         /**
+         * This is a simple (debug) entity; it is used to debug the tracking for
+         * ball progress through the maze by leaving a trail of "bread crumbs".
+         */
+        var Marker = (function (_super) {
+            __extends(Marker, _super);
+            /**
+             * Construct a new arrow entity that will render on the stage provided.
+             *
+             * This entity is always in a continuously animated state, although the
+             * animation may be only a single frame.
+             *
+             * @param {Stage}          stage     the stage that we use to render
+             * ourselves
+             * @param {ArrowType}      arrowType the type of arrow to create
+             * @param {ArrowDirection} direction the direction the arrow is facing
+             */
+            function Marker(stage, maze) {
+                // Invoke the super; note that this does not set a position because
+                // that is set by whoever created us. We set our dimensions based on
+                // the maze provided. Note that this requires that the maze know
+                // the cell size, which it can only know after all prelods are
+                // compelted.
+                _super.call(this, stage, "marker");
+                this.makeRectangle(maze.cellSize, maze.cellSize);
+                // ALl of our rendering is handled by the super class, so all we
+                // have to do is set the color we want to render with.
+                this._debugColor = 'white';
+            }
+            return Marker;
+        }(game.MazeCell));
+        game.Marker = Marker;
+    })(game = nurdz.game || (nurdz.game = {}));
+})(nurdz || (nurdz = {}));
+var nurdz;
+(function (nurdz) {
+    var game;
+    (function (game) {
+        /**
          * This is used to indicate what type of ball this is. This is just for
          * visual identification on the board.
          */
---
+++ ts/game/entity/Marker.ts
@@ -0,0 +1,35 @@
+module nurdz.game
+{
+    /**
+     * This is a simple (debug) entity; it is used to debug the tracking for
+     * ball progress through the maze by leaving a trail of "bread crumbs".
+     */
+    export class Marker extends MazeCell
+    {
+        /**
+         * Construct a new arrow entity that will render on the stage provided.
+         *
+         * This entity is always in a continuously animated state, although the
+         * animation may be only a single frame.
+         *
+         * @param {Stage}          stage     the stage that we use to render
+         * ourselves
+         * @param {ArrowType}      arrowType the type of arrow to create
+         * @param {ArrowDirection} direction the direction the arrow is facing
+         */
+        constructor (stage : Stage, maze : Maze)
+        {
+            // Invoke the super; note that this does not set a position because
+            // that is set by whoever created us. We set our dimensions based on
+            // the maze provided. Note that this requires that the maze know
+            // the cell size, which it can only know after all prelods are
+            // compelted.
+            super (stage, "marker");
+            this.makeRectangle (maze.cellSize, maze.cellSize);
+
+            // ALl of our rendering is handled by the super class, so all we
+            // have to do is set the color we want to render with.
+            this._debugColor = 'white';
+        }
+    }
+}
