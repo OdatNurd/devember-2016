9a6c99624aee247a061dabd3315c9fba9e3dee0f
Terence Martin <poindexter@nurdz.com>
2016-12-08 16:06:51 -0800

    MazeCell refactor
    For upcoming changes, the API methods in MazeCell that are used to
    move the ball have been renamed and had their signatures modified. The
    rename is just to make the code more readable and to find errors
    faster.
    
    changeBallLocation() has become ballCollsion(), and gets the maze, ball
    and location of where the collision is happening. Instead of modifying
    the Point given if it needs to modify the location, it returns either
    null or a new location.
    
    didChangeDirection() has become didMoveBall(), and gets the ball that
    it moved.
    
    touchingBall() has become ballTouch(), and like ballCollision() gets
    the maze, ball and location of the touch.
    
    At the current time the old API is still there and in use.

--- js/game.js
+++ js/game.js
@@ -182,20 +182,6 @@ var nurdz;
                 return true;
             };
             /**
-             * This method will be invoked on this maze cell when the ball enters
-             * the cell that it contains.
-             *
-             * This can only happen if blocksBall() returns false, as otherwise the
-             * ball would be blocked from entering this entity.
-             *
-             * The position of the ball (and thus of this grid entity) is passed in.
-             * The ball will be shifted to the location of this point when this call
-             * completes, which allows this entity to move this ball when it touches
-             * it.
-             */
-            MazeCell.prototype.touchingBall = function (ballPosition) {
-            };
-            /**
              * Returns a determination on where the ball would go if it was not
              * allowed to enter this cell. This only gets invoked in the case where
              * the ball is blocked from entering the cell (i.e. blockBall() returns
@@ -217,12 +203,96 @@ var nurdz;
                 return false;
             };
             /**
+             * If this Maze cell blocks the ball (blocksBall() returns true), then
+             * this method will be invoked to indicate that the ball tried to enter
+             * the same maze cell as it is currently occupies..
+             *
+             * The collision is informed of the maze that it is contained in, the
+             * ball that it is colliding with, and the location in the maze that the
+             * collision is happening at (i.e. the location in the maze of this
+             * MazeCell).
+             *
+             * If desired, the position of the ball can be modified by returning a
+             * point that represents the new position in the maze. Otherwise, the
+             * ball is left at the current location.
+             *
+             * This position may or may not be used by the engine; if it is,
+             * didMoveBall() will be invoked to tell us that the ball provided was
+             * actually moved.
+             *
+             * @param   {Maze}  maze     the maze containing us and the ball
+             * @param   {Ball}  ball     the ball that is coliding with us
+             * @param   {Point} location the location in the maze that we are at
+             *
+             * @returns {Point}          if non-null, this is the position that the
+             * ball should be moved to in response to colliding with us; a return
+             * value of null indicates the ball should stay where it is.
+             */
+            MazeCell.prototype.ballCollision = function (maze, ball, location) {
+                return null;
+            };
+            /**
              * This is invoked after a call to changeBallLocation() that returned
              * true if the ball was actually changed to that location. That allows
              * this cell to do something in response to having successfully moved
              * the ball.
              */
             MazeCell.prototype.didChangeDirection = function () {
+            };
+            /**
+             * This is invoked after a call to ballCollsiion() indicated that the
+             * ball location should be changed as a result of colliding with us, and
+             * the position of the ball was actually changed as a result of what we
+             * said.
+             *
+             * The ball that was moved is provided to the call to indicate which
+             * ball was the one that moved.
+             *
+             * @param {Ball} ball the ball that we moved
+             */
+            MazeCell.prototype.didMoveBall = function (ball) {
+            };
+            /**
+             * This method will be invoked on this maze cell when the ball enters
+             * the cell that it contains.
+             *
+             * This can only happen if blocksBall() returns false, as otherwise the
+             * ball would be blocked from entering this entity.
+             *
+             * The position of the ball (and thus of this grid entity) is passed in.
+             * The ball will be shifted to the location of this point when this call
+             * completes, which allows this entity to move this ball when it touches
+             * it.
+             */
+            MazeCell.prototype.touchingBall = function (ballPosition) {
+            };
+            /**
+             * This is invoked when the ball enters the same cell as this maze
+             * entity (which measns that a call to blocksBall() returned false) to
+             * tell us that the ball has actually entered our location.
+             *
+             * The collision is informed of the maze that it is contained in, the
+             * ball that is touching us, and the location in the mazr that the touch
+             * is happening at (i.e. the location in the maze of this MazeCell).
+             *
+             * If desired, the position of the ball can be modified by returning a
+             * pooint that represents the new position in the maze; otherwise the
+             * ball is left at the current location.
+             *
+             * This position may or may not be used by the engine. If the position
+             * of the ball is changed, a touch event will not fire if the ball was
+             * placed on top of another entity that supports this call.
+             *
+             * @param   {Maze}  maze     the maze containing us and the ball
+             * @param   {Ball}  ball     the ball that is touching us
+             * @param   {Point} location the location in the mazer that we are at
+             *
+             * @returns {Point}          if non-null, this is the position that the
+             * ball should be moved to in response to touching us; a return value of
+             * null indicates that the ball should stay where it is
+             */
+            MazeCell.prototype.ballTouch = function (maze, ball, location) {
+                return null;
             };
             return MazeCell;
         }(game.Entity));
